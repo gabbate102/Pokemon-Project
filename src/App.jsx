73bc428c86992c3d205c9b31a4d101802fb1cde0
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-02-08 13:16:41 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+    </>
+  )
+}
+
+export default App
