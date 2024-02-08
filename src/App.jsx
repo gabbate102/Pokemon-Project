f51ff267b6044c47346ba28661b3c8abe2453289
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-02-08 14:12:56 -0500

    Started on layout, added search functionality and passed props back to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import NavBar from "./Layout/Navbar"
+import PokeSearch from "./PokeSearch"
+import { useState } from "react";
+
 function App() {
 
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
   return (
-    <>
-      <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-    </>
+    <main className="mx-auto bg-[#363B81] ">
+      <NavBar />
+      {/*Main Section */}
+      <section className="px-6 py-8 flex h-96 items-center justify-center ">
+        <div className="gap-4 flex flex-col">
+          <h1 className="text-xl">Search for your favorite Pokemon</h1>
+          <div className="flex">
+            <PokeSearch setSelectedPokemon={setSelectedPokemon} />
+          </div>
+        </div>
+      </section>
+
+      {selectedPokemon &&
+        <div>{selectedPokemon.name}</div>
+      }
+
+      {/* <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> */}
+    </main>
   )
 }
 
---
+++ src/Layout/Navbar.jsx
@@ -0,0 +1,25 @@
+export default function NavBar() {
+    return (
+        <div className="navbar bg-[#363B81] ">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">Pokemon Selector</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Link</a></li>
+                    <li>
+                        <details>
+                            <summary>
+                                Parent
+                            </summary>
+                            <ul className="p-2 bg-base-100 rounded-t-none">
+                                <li><a>Link 1</a></li>
+                                <li><a>Link 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
