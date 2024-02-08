6f5c88292d7eaa7d723139d90aba9ec037676d01
gabbate102 <gabbate2011@gmail.com>
2024-02-08 14:14:32 -0500

    Started pokemon card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Pokecard from './Pokecard.jsx'
 function App() {
 
   return (
     <>
       <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+      <Pokecard/>
     </>
   )
 }
---
+++ src/Pokecard.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function Pokecard(props) {
+  return (
+  <div>
+    <div class="fixed pokecard">
+      <div>title section</div>
+      <div>photo section</div>
+      <div>details section</div>
+      <div>bottom section</div>
+    </div>
+  </div>
+  )
+}
