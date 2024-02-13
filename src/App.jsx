643c6f17e823b1add11d8bba1a3bae8862b1a00f
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-02-13 14:15:59 -0500

    updated stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
 import NavBar from "./Layout/Navbar"
 import PokeSearch from "./PokeSearch"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [favorites, setFavorites] = useState([]);
+
+  // get all favorites from local storage and set them to state
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(favorites);
+  }, []);
 
   return (
     <main className="mx-auto bg-[#363B81] ">
       <NavBar />
       {/*Main Section */}
-      <section className="px-6 py-8 flex items-center justify-center ">
-        <div className="gap-4 flex flex-col items-center">
-          <h1 className="text-xl font-bold">Search for your favorite Pokemon</h1>
+      <section className="flex items-center">
+        <div className="gap-4 flex flex-col">
+          <h1 className="text-xl font-light text-center">Search for your favorite Pokemon</h1>
           <div className="">
-            <PokeSearch setSelectedPokemon={setSelectedPokemon} />
+            <PokeSearch setSelectedPokemon={setSelectedPokemon} setFavorites={setFavorites} />
           </div>
         </div>
       </section>
 
-      {selectedPokemon &&
-        <div>{selectedPokemon.name}</div>
-      }
+      <section className="bg-gray-800 items-start">
+        <div>
+          <h1 className="font-light text-xl">Your favorite pokemon</h1>
+          <div className="grid grid-cols-8 gap-8">
+            {favorites && favorites.map((pokemon, idx) => {
+              return (
 
-      {/* <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /> */}
+                <div className="flex flex-col items-center" idx={idx}>
+                  <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.index}.png`} width={80} />
+                  <p>{pokemon.name}</p>
+                </div>
+
+              )
+            })
+            }
+          </div>
+        </div>
+      </section>
+
+
     </main>
   )
 }
