1d2929b989a4a74249f0ec7d25349d671e39e8c5
gabbate102 <gabbate2011@gmail.com>
2024-02-16 20:39:48 -0500

    Refactor Pokecard component to fetch selected Pokemon data from API

--- src/pokecard.jsx
+++ src/pokecard.jsx
@@ -3,19 +3,18 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function Pokecard({selectedPokemon}) {
-  console.log(selectedPokemon)
   const [pokemon, setPokemon] = useState([]);
   // get data for selected pokemon from api
   useEffect(() => {
     if (selectedPokemon != undefined) {
       fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}/`)
-          .then((res) => res.json())
-          .then((data) => {
-              setPokemon(data);
-              console.log(data);
-          });
+        .then((res) => res.json())
+        .then((data) => {
+            setPokemon(data);
+            console.log(data);
+        });
     }
-  }, []);
+  }, [selectedPokemon]);
 
   return (
   <div>
@@ -25,7 +24,7 @@ export default function Pokecard({selectedPokemon}) {
         <p class="card-title">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
         <p class="hp">HP: {pokemon.stats[0]["base_stat"]}</p>
       </div>
-      <div class="w-[214] photo-section">
+      <div class="w-[214] photo-section flex">
         <img className="w-72" src={pokemon.sprites["front_shiny"]}></img>
       </div>
       <div class="flex-col justify-center mt-2">
